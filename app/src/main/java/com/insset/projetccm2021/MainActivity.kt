7e8bdeafdb788ec8b1cae6fa3cb8f2033fb8712f
Romainpst <pvp02430@hotmail.fr>
2021-12-13 22:39:51 +0100

    added homepage

--- app/src/main/java/com/insset/projetccm2021/MainActivity.kt
+++ app/src/main/java/com/insset/projetccm2021/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.insset.projetccm2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.insset.projetccm2021.list.view.ApiListActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val buttonToApi: Button = findViewById(R.id.buttonToApi)
+
+        buttonToApi.setOnClickListener(){
+            val intent = Intent(this, ApiListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
