cb3d2aceedab49ef127a0de7fbf797f6c1c65b3e
Yoann Paquereau <yoann.paquereau@gmail.com>
2021-12-14 13:47:06 +0100

    feat: make register / login works

--- app/src/main/java/com/insset/projetccm2021/MainActivity.kt
+++ app/src/main/java/com/insset/projetccm2021/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.insset.projetccm2021.auth.view.LoginActivity
 import com.insset.projetccm2021.list.view.ApiListActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    val buttonToApi: Button = findViewById(R.id.buttonToApi)
+        val buttonToApi: Button = findViewById(R.id.buttonToApi)
+        val buttonToAuth: Button = findViewById(R.id.buttonToAuth)
 
-        buttonToApi.setOnClickListener(){
+        buttonToApi.setOnClickListener() {
             val intent = Intent(this, ApiListActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToAuth.setOnClickListener() {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
